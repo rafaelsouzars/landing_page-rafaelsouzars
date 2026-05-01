8d5a0f383528a039fb8176aaa4d82fc02e9a39be
rafaelsouzars <rafael21.santos@hotmailcom>
2026-05-01 12:01:43 -0300

    :lipstick: feat: add button style Description: Added button style and animations. Author: @rafaelsouzars

--- src/components/Card/CardProfile.jsx
+++ src/components/Card/CardProfile.jsx
@@ -1,11 +1,12 @@
 //import React from 'react'
 //import Card from './Card.jsx'
 import profileImg from '../../assets/img/profile_photo.jpg'
+import Button from '../Button/Button.jsx'
 import './Card.css'
 
 const CardProfile = ({ photo, children }) => {
   return (
-    <div className='profile'>
+    <section className='profile'>
       <figure>
         <img className='photo' src={photo ?? profileImg} alt='Profile photo rafaelsouzars'/>
       </figure>
@@ -13,14 +14,18 @@ const CardProfile = ({ photo, children }) => {
         Rafael Souza
       </figcaption>
       <div className='release'>
-        Release
+        <p className='font-size align-left secondary-text-color'>
+          Sou um desenvolvedor apaixonado por aplicações
+          <span className='font-size highlight'> Web, Desktop, Browser Extensions, API, Scrapping e Engenharia Reversa</span>.
+          Gosto de participar de projetos desde o levantamento de requisitos até o desenvolvimento.
+        </p>
       </div>
       <div className='contact'>
-        Contact
+        <Button className='secondary-button'>Contate-me</Button>
       </div>
       
       {children}
-    </div>
+    </section>
   )
 }
 
